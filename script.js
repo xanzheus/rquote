1ea0f5d734cf85224741b8567754c74aec675d25
Ankita Tanti <ankita@creolestudios.com>
2019-11-19 18:52:27 +0530

    new qoutes api

---
+++ script.js
@@ -0,0 +1,16 @@
+var $ = jQuery;
+jQuery(document).ready( () => {
+ getNewQuote();
+ $('.get-new-quote').on('click',() => {
+ 	getNewQuote();
+ });
+});
+getNewQuote = () => {
+	$.ajax({
+		url: "https://api.quotable.io/random",               
+		success: function(result) { 
+                   $('.qoute').text(result.content);
+                   $('.author-name').text(result.author);
+			    }	    	    
+	}); 
+}
