3a2b224d62883a2081e18e52df70046fcefe19e4
Ronak vanpariya <vanpariyar@gmail.com>
2019-12-15 23:26:27 +0530

    ehnhancement

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ jQuery(document).ready( () => {
  });
 });
 getNewQuote = () => {
+	function getRandomIntInclusive(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+	}
 	$.ajax({
 		url: "https://vanpariyar.github.io/get-new-quote/rendomQuote.json",               
 		success: function(result) { 
-                   $('.qoute').text('" '+result.content+' "');
-				   $('.author-name').text(result.author);
-				   console.log(result);
+				const quoteNumber = getRandomIntInclusive(0,103);
+				$('.qoute').text('" '+result[quoteNumber].quote+' "');
+				$('.author-name').text(result[quoteNumber].author);
 			    }	    	    
 	}); 
 }
